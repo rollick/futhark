3eff99cad7b94399d9dcf8602587a5e1a15971f6
Mark Gallop <rollick@users.noreply.github.com>
2013-11-28 11:02:22 +0100

    Update ruby-deb.sh

--- ruby-deb.sh
+++ ruby-deb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-apt-get install ruby rubygems
+apt-get install ruby1.8 rubygems1.8
 
 gem1.8 install fpm --no-ri --no-rdoc
 
@@ -17,16 +17,14 @@ rm -rf /tmp/ruby20
 
 ./configure --prefix=/usr --with-iconv-dir=/usr/local && make && make install DESTDIR=/tmp/ruby20
 
-  
 /var/lib/gems/1.8/bin/fpm -s dir -t deb -n ruby20 -v 2.0 --description \
   "Ruby 2.0 stable package" -C /tmp/ruby20 \
   -p ruby2.0-VERSION_ARCH.deb -d "libstdc++6 (>= 4.4.3)" \
-  -d "libc6 (>= 2.6)" -d "libffi5 (>= 3.0.4)" -d "libgdbm3 (>= 1.8.3)" \
+  -d "libc6 (>= 2.6)" -d "libffi-dev (>= 3.0.4)" -d "libgdbm3 (>= 1.8.3)" \
   -d "libncurses5 (>= 5.7)" -d "libreadline6 (>= 6.1)" \
   -d "libssl0.9.8 (>= 0.9.8)" -d "zlib1g (>= 1:1.2.2)" \
   -d "libyaml-0-2 (>= 0.1.3)" \
   usr/bin usr/lib usr/share/man usr/include
-
   
 # Deb is ready!
 
